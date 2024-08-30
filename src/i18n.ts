61ee9cd29d1bad4d168455d3c08117d71bf503fb
andrew-kras <andrewkrasj@mail.ru>
2024-08-30 15:56:17 +0300

    LanguageDetector was added

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 const resources = {
     en: {
@@ -55,11 +56,16 @@ const resources = {
 };
 
 i18n
+    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         resources,
         lng: 'ru',
         fallbackLng: 'en',
+        detection: {
+            order: ['navigator', 'htmlTag', 'cookie', 'localStorage', 'path', 'subdomain'],
+            caches: ['cookie', 'localStorage']
+        },
         interpolation: {
             escapeValue: false
         }
